5b03fa151d545090241b092075a6137504c95b9d
Massimo Peri <massimoperi@gmail.com>
2012-10-14 23:13:04 +0200

    Add credits for Appirater

--- Smokeless/CreditsViewController.m
+++ Smokeless/CreditsViewController.m
@@ -130,6 +130,19 @@
     thanksCredit2.font = [UIFont systemFontOfSize:15.0];
     thanksCredit2.text = MPString(@"Matej Bukovinski for MBProgressHUD");
 
+    UILabel *thanksCredit3 = [[UILabel alloc] initWithFrame:CGRectMake(0.0,
+                                                                       8*LABEL_HEIGHT + LABEL_PADDING + 2*SECTION_PADDING,
+                                                                       320.0,
+                                                                       LABEL_HEIGHT)];
+    thanksCredit3.backgroundColor = [UIColor clearColor];
+    thanksCredit3.textAlignment = UITextAlignmentCenter;
+    thanksCredit3.textColor = [UIColor blackColor];
+    thanksCredit3.shadowColor = [UIColor colorWithWhite:0.850
+                                                  alpha:1.000];
+    thanksCredit3.shadowOffset = CGSizeMake(0.0, 1.0);
+    thanksCredit3.font = [UIFont systemFontOfSize:15.0];
+    thanksCredit3.text = MPString(@"Arash Payan for Appirater");
+    
     // create view hierarchy
     [self.view addSubview:dndTitle];
     [self.view addSubview:dndCredit];
@@ -139,6 +152,7 @@
     [self.view addSubview:thanksTitle];
     [self.view addSubview:thanksCredit1];
     [self.view addSubview:thanksCredit2];
+    [self.view addSubview:thanksCredit3];
 }
 
 @end
