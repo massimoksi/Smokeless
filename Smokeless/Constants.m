24eceb1452262c8dcea29b5a1608719a9da403be
Massimo Peri <massimoperi@gmail.com>
2015-02-15 09:23:09 +0100

    Minor changes

--- Smokeless/Constants.m
+++ Smokeless/Constants.m
@@ -9,14 +9,14 @@
 #import "Constants.h"
 
 
-NSString * const LastCigaretteKey = @"LastCigarette";
-NSString * const HabitsKey = @"Habits";
-NSString * const HabitsQuantityKey = @"HabitsQuantity";
-NSString * const HabitsUnitKey = @"HabitsUnit";
-NSString * const HabitsPeriodKey = @"HabitsPeriod";
-NSString * const PacketSizeKey = @"PacketSize";
-NSString * const PacketPriceKey = @"PacketPrice";
-NSString * const ShakeEnabledKey = @"ShakeEnabled";
-NSString * const NotificationsEnabledKey = @"NotificationsEnabled";
-NSString * const UserSettingsImportedKey = @"UserSettingsImported";
+NSString * const LastCigaretteKey               = @"LastCigarette";
+NSString * const HabitsKey                      = @"Habits";
+NSString * const HabitsQuantityKey              = @"HabitsQuantity";
+NSString * const HabitsUnitKey                  = @"HabitsUnit";
+NSString * const HabitsPeriodKey                = @"HabitsPeriod";
+NSString * const PacketSizeKey                  = @"PacketSize";
+NSString * const PacketPriceKey                 = @"PacketPrice";
+NSString * const ShakeEnabledKey                = @"ShakeEnabled";
+NSString * const NotificationsEnabledKey        = @"NotificationsEnabled";
+NSString * const UserSettingsImportedKey        = @"UserSettingsImported";
 NSString * const HasUpdatedLastCigaretteDateKey = @"HasUpdatedLastCigaretteDate";
